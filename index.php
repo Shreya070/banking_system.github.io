f5aa2e67dde539a427e25e0edb54e71fcca1a848
Shreya Gupta <shreya7sdl@gmail.com>
2022-01-10 17:00:35 +0530

    banking_sysyem

---
+++ index.php
@@ -0,0 +1,72 @@
+<?php
+include "style.php";
+?> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Basic Banking System</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/fontawesome.min.css" integrity="sha512-Rcr1oG0XvqZI1yv1HIg9LgZVDEhf2AHjv+9AuD1JXWGLzlkoKDVvE925qySLcEywpMAYA/rkg296MkvqBF07Yw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+ 
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+<body class="bg-light">
+
+<nav class="navbar navbar-expand-sm bg-light navbar-light">
+  <a class="navbar-brand" href="index.php">Home</a>
+</nav>
+<div class="container mt-5">
+<h1 class="text-center"> BASIC BANKING SYSTEM</h1>
+
+
+         <h2 class="text-center">TRANSFER YOUR MONEY EASILY AND SECURELY.</h2>
+         
+</div>
+<br>
+<br>
+<br>
+<br>
+<div class="container pt-3">
+<div class="row justify-content-center">
+  <a href="view_users.php" class="btn btn-outline-dark btn-lg  ">View All Users</a>
+</div>
+</div>
+<br>
+<br>
+<div class="container pt-3">
+<div class="row justify-content-center">
+  <a href="transfer.php" class="btn btn-outline-dark btn-lg">Transfer Money</a>
+</div>
+</div>
+<br>
+<br>
+<div class="container pt-3">
+<div class="row justify-content-center">
+  <a href="transaction.php" class="btn btn-outline-dark btn-lg">All Transactions</a>
+</div>
+<footer>
+      <hr>
+      <div class="row text-center">
+        <div class="col-md-6">
+          <p>Project By: Shreya Gupta</p>
+
+        </div>
+        <div class="col-md-6 text-center">
+            <ul class="social-icons">
+              Contact Me:
+              <li><a target="_blank" class="github" href="https://github.com/Shreya070"><i class="fab fa-github fa-2x"></i></a></li>
+
+              <li><a target="_blank" class="linkedin" href="https://linkedin.com/in/shreya-gupta-972b881a0/"><i class="fab fa-linkedin fa-2x"></i></a></li>   
+            </ul>
+        </div>
+      </div>
+    </footer>
+</div>
+</body>
+</html>  
---
+++ style.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+    <head?>
+        <title></title>
+    
+<style>
+   
+    html{
+    padding: 0;
+    margin: 0;
+}
+
+body{
+    font-family: 'Roboto', sans-serif;
+    background-color: #fa0a0a;
+    background-image:url('em.jpg');
+}
+
+.bg-light{
+    background-color: #05e749!important;
+}
+
+.navbar-brand{
+    font-family: 'Sansita Swashed', cursive;
+    font-size: 1.5rem;
+    color: #080106!important;
+   
+}
+
+.nav-link{
+    color: #0b0fee!important;
+    font-family: 'Noto Sans JP', sans-serif;
+    font-weight:bold;
+}
+
+.nav-link:hover{
+    background-color: #ee0c0c;
+}
+.container a{
+    color:black; 
+    border-color:black;
+    font-weight:bold;
+}
+footer{
+    background-color:  #edecf0;
+    background-image:url('em.jpg');
+}
+
+.social-icons li{
+    list-style: none;
+    display: inline;
+    padding: 0.5rem;
+}
+
+.github{
+    color: #0b1af3;
+}
+
+.linkedin{
+    color: #0616eb;
+}
+
+.google{
+    color: #faf5f4;
+}
+}
+</style>
+</head>
+    </body>
+    </html> 
